1bd1493b6632656d55424809e3ffa30667c12275
Anton Nikeev <antnick45@gmail.com>
2022-01-04 14:22:43 +0300

    Packages update.

--- webpack.config.js
+++ webpack.config.js
@@ -58,7 +58,7 @@ Encore
     // enables Sass/SCSS support
     .enableSassLoader()
 
-    .enableVueLoader()
+    .enableVueLoader(() => {}, { runtimeCompilerBuild: false })
 
 // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
